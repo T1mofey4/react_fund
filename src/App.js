c796bcea19b4f64622acadea8c5a5610871cb518
Timofey <timofeyvalerievich@gmail.com>
2022-07-29 17:33:15 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from "react";
+import Counter from "./components/counter";
+import ClassCounter from "./components/classComponent";
+import "./styles/App.css";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/button/input/MyInput";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: '1.', title: 'Javascript', body: 'Description'},
+    {id: '2.', title: 'Javascript 2', body: 'Description'},
+    {id: '3.', title: 'Javascript 3', body: 'Description'}
+  ])
+
+  const [title, setTitle] = useState('')
+
+  const [body, setBody] = useState('')
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    const newPost = {
+      id: Date.now(),
+      title,
+      body
+    }
+    setPosts([...posts, newPost])
+    setTitle('')
+    setBody('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <MyInput 
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста" 
+        />
+        <MyInput
+          value={body}
+          onChange={e => setBody(e.target.value)}
+          type="text"
+          placeholder="Описание поста" 
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts = {posts} title = 'Посты про JS'/>
     </div>
   );
 }
